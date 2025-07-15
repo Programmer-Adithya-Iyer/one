ed4245bbc1074c00dc36f9a9d78fd3355d6e178e
Programmer-Adithya-Iyer <work.adithyaiyer@gmail.com>
2025-07-15 17:08:51 +0000

    Getting to work on the card solo project
    from freecodecamp.org

--- src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Footer.css";
-
-const Footer = () => {
-	return <footer>Copyright 20xx Iyer development. All rights reserved.</footer>;
-};
-
-export default Footer;
